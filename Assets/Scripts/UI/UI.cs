245aabcf4eaa55b03aea79e2c61e361e192e66f2
kiendeptrai24 <kienmiu2004@gmail.com>
2025-03-14 22:16:09 +0700

    fade screen

--- Assets/Scripts/UI/UI.cs
+++ Assets/Scripts/UI/UI.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
@@ -12,6 +14,8 @@ public class UI : MonoBehaviour
 
 
     [SerializeField] private GameObject[] UI_Elements;
+    [Header("Fade Image")]
+    [SerializeField] private Image fadeImage;
 
     private void Awake()
     {
@@ -22,6 +26,7 @@ public class UI : MonoBehaviour
     }
     private void Start() {
         AssignInputUI();
+        StartCoroutine(ChangeImageAlpha(0,1.5f,null));
     }
     public void SwitchTo(GameObject uiToSwitchOn)
     {
@@ -32,24 +37,12 @@ public class UI : MonoBehaviour
         uiToSwitchOn.SetActive(true);
 
     }
-    public void SwitchToInGameUI()
-    {
-        SwitchTo(inGameUI.gameObject);
-        GameManager.Instance.GameStart();
-        ControlsManager.Instance.SwitchToCharactorControls();
-        Time.timeScale = 1;
-
-    }
+    public void SwitchToInGameUI() => StartCoroutine(StartGameSequence());
     public void SwitchToGameOverUI(string message = "GAME OVER!!!")
     {
         SwitchTo(gameOverUI.gameObject);
         gameOverUI.ShowGameOverMessage(message);
-        
     }
-    public void StartTheGame() => SwitchToInGameUI();
-    public void QuitTheGame() => Application.Quit();
-    public void RestartTheGame() => GameManager.Instance.ReStartScene();
-
     public void PauseSwitch()
     {
         bool gamePause = pauseUI.activeSelf;
@@ -66,6 +59,41 @@ public class UI : MonoBehaviour
             ControlsManager.Instance.SwitchToUIControls();
             TimeManager.Instance.PauseTime();
         }
+    }
+
+    public void StartTheGame() => SwitchToInGameUI();
+    public void QuitTheGame() => Application.Quit();
+    public void RestartTheGame() => StartCoroutine(ChangeImageAlpha(1,1f,GameManager.Instance.ReStartScene));
+    
+
+    private IEnumerator StartGameSequence()
+    {
+        StartCoroutine(ChangeImageAlpha(1,1,null));
+        Debug.Log("turn on");
+        yield return new WaitForSeconds(1);
+        SwitchTo(inGameUI.gameObject);
+        GameManager.Instance.GameStart();
+        ControlsManager.Instance.SwitchToCharactorControls();
+        StartCoroutine(ChangeImageAlpha(0,1,null));
+        Debug.Log("turn off");
+
+    }
+    private IEnumerator ChangeImageAlpha(float targetAlpha, float duration, Action onComplete)
+    {
+        float time = 0;
+        Color currentColor = fadeImage.color;
+        float startAlpha = currentColor.a;
+        while(time < duration)
+        {
+            time += Time.deltaTime;
+            float alpha = Mathf.Lerp(startAlpha,targetAlpha,time/duration);
+
+            fadeImage.color = new Color(currentColor.r,currentColor.g,currentColor.b,alpha);
+
+            yield return null;
+        }
+        fadeImage.color = new Color(currentColor.r,currentColor.g,currentColor.b,targetAlpha);
+        onComplete?.Invoke();
     }
     private void AssignInputUI()
     {
